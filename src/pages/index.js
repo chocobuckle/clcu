ff6cc586aa008bef02d53a2478f01b627249675b
Conor Doyle <contact@conordoyle.io>
2018-08-17 14:44:24 +0100

    Add horizontal rule to bottom of page

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,6 +64,7 @@ function IndexPage({ data }) {
         mobileAndDesktopImageWidth="100vw"
         textContent="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et."
       />
+      <HR style={{ marginBottom: '4vw'}} />
     </Wrapper>
   );
 }
