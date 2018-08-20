9abd45eea2d60701d13a271a86b2135565b0fb3c
Conor Doyle <contact@conordoyle.io>
2018-08-21 00:40:31 +0100

    Tweak style of 'TabletAndDesktopHeader' component

--- src/components/Header/TabletAndDesktopHeader/index.js
+++ src/components/Header/TabletAndDesktopHeader/index.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
-import { backgroundGrey } from 'sharedStyles';
+import { backgroundGrey, desktop } from 'sharedStyles';
 
 const Wrapper = styled.div`
   background: linear-gradient(
     180deg,
-    rgba(255, 255, 255, 1) 83.75%,
-    ${backgroundGrey} 83.75%,
+    rgba(255, 255, 255, 1) 82.7%,
+    ${backgroundGrey} 82.7%,
     rgba(255, 255, 255, 1) 100%
   );
-  padding: 12.5% 0 3.125%;
+  padding: 9.25% 0 3.125%;
+  width: 100%;
 `;
 
 const TabletAndDesktopImg = styled(Img)`
   width: 70.5%;
-  margin: 0 auto;
+  margin: 0 auto -16.3vw;
+
+  @media screen and (min-width: ${desktop}) {
+    margin: 0 auto -162.674px;
+  }
 `;
 
 const piggyImgStyle = {
-  position: 'absolute',
+  position: 'relative',
   width: '16%',
   bottom: '35%',
   left: '-3%',
